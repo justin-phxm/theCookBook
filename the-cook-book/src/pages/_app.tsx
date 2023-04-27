589423f874b3148b7e6e34b75533daf0fea6fbf5
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-04-27 13:30:04 -0600

    add Outfit font

--- the-cook-book/src/pages/_app.tsx
+++ the-cook-book/src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Outfit } from 'next/font/google'
+
+const outfit = Outfit({subsets: ['latin'],})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <main 
+    className = {outfit.className}
+    >
+      <Component {...pageProps} />
+    </main>
+  )
 }
