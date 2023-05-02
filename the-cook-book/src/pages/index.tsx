957ed0ffae61157d4cf205626ac5e4dc5ca2c8e8
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-02 12:27:37 -0600

    add call to action

--- the-cook-book/src/pages/index.tsx
+++ the-cook-book/src/pages/index.tsx
@@ -3,13 +3,14 @@ import NavBar from '../components/navBar'
 import CreateRecipe from '../components/createRecipe'
 import Testimony from '../components/testimony'
 import Footer from '../components/footer'
-
+import CallToAction from '../components/callToAction'
 export default function Home() {
   return (
     <main className='min-h-screen bg-[#F4FFF1]'>
       <NavBar />
       <CreateRecipe />
       <Testimony />
+      <CallToAction />
       <Footer />
     </main>
   )
