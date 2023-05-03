ab02a830e8e885fee2c66f927f3f68b83911e687
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-03 14:53:54 -0600

    horizontal scrolling fix

--- the-cook-book/src/pages/index.tsx
+++ the-cook-book/src/pages/index.tsx
@@ -8,7 +8,7 @@ import Circle from '../components/circularDecoration'
 
 export default function Home() {
   return (
-    <main className='min-h-screen bg-[#F4FFF1]'>
+    <main className='min-h-screen bg-[#F4FFF1] overflow-hidden relative'>
       <Circle />
       <NavBar />
       <FeatureBlock />
