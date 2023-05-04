b1d65f92d520405231f3070073028dc779f73470
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-03 18:19:13 -0600

    update contact link

--- the-cook-book/src/pages/contact.tsx
+++ the-cook-book/src/pages/contact.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import Head from 'next/head'
+import Layout from '../components/layout'
+import Image from 'next/image'
+import aboutImage from '../../public/12.svg'
 
 export default function contact() {
-  return (
-    <div>contact</div>
-  )
+  return (<>
+    <Layout>
+      <Head>
+        <title>Contact</title>
+      </Head>
+      <div className="text-3xl font-bold">Contact</div>
+      <Image src={aboutImage} alt="" width={1000}/>
+    </Layout>
+    </>)
 }
