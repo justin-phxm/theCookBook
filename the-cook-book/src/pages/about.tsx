c81fc9265ce6a0e40b8123a6c6978f91ba888d65
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-03 18:16:12 -0600

    update about page

--- the-cook-book/src/pages/about.tsx
+++ the-cook-book/src/pages/about.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
 import Head from 'next/head'
 import Layout from '../components/layout'
+import Image from 'next/image'
+import aboutImage from '../../public/12.svg'
 
 export default function about() {
   return (<>
@@ -8,7 +9,8 @@ export default function about() {
       <Head>
         <title>About</title>
       </Head>
-      <div>about</div>
+      <div className="text-3xl font-bold">About</div>
+      <Image src={aboutImage} alt="" width={1000}/>
     </Layout>
     </>)
 }
