488f36246ae76b56cb439cd43939e473b57680ca
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-07 17:08:17 -0600

    create ingredients component

---
+++ the-cook-book/src/components/recipe/Ingredients.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import IFoodItem from '../FoodInterface'
+
+export default function Ingredients({foodItem}: {foodItem: IFoodItem}) {
+    {console.log(foodItem.ingredients)}
+
+    return (
+    <div className="bg-white rounded-md h-3/4 p-2 overflow-auto resize">
+        <div className=" font-bold text-lg text-">
+            Ingredients:
+        </div>
+        <ul className="resize">
+            {foodItem.ingredients?.map((ingredient, index) => (
+                <li className="flex flex-row font-normal" key={index}>
+                    • {ingredient}
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
