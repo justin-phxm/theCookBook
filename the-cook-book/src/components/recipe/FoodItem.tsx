487298fad9312e71b0f11df4f76362fcfc189816
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-07 15:58:23 -0600

    FoodItem opacity on hover

--- the-cook-book/src/components/recipe/FoodItem.tsx
+++ the-cook-book/src/components/recipe/FoodItem.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import Image from 'next/image'
 import placeholder from "../../../public/placeholder-image.png"
+import IFoodItem from '../FoodInterface'
 
 const imageLoader = ({ src, width, quality }:{src?:string, width?:number, quality?:number}) => {
   return `https://themealdb.com/${src}?w=${width}&q=${quality || 1}`;
 };
 
-export default function FoodItem({FoodItem}:{FoodItem:{name?:string, image?:string, id?:string}}) {
+export default function FoodItem({FoodItem}:{FoodItem:IFoodItem}) {
 
   return (
     <>
-      <button className="h-full aspect-square hover:underline w-fit flex flex-col items-center xl:w-56 2xl:w-64 mx-2 bg-gray-200 rounded-lg border justify-center
+      <button className="h-full group aspect-square hover:underline w-fit flex flex-col items-center xl:w-56 2xl:w-64 mx-2 bg-gray-200 rounded-lg border justify-center
        border-white shadow-md dark:border-gray-700  dark:bg-gray-800">
             {FoodItem.image ? 
-            <Image className="h-5/6 rounded-md p-3"
+            <Image className="h-5/6 rounded-md p-3 group-hover:opacity-75"
                 loader={imageLoader}
                 src={FoodItem.image}
                 alt="No image"
@@ -21,7 +22,7 @@ export default function FoodItem({FoodItem}:{FoodItem:{name?:string, image?:stri
                 height={500}
             />
           :
-            <Image className="w-1/2 rounded-md shadow-lg"
+            <Image className="w-1/2 rounded-md shadow-lg opacity-75"
             src={placeholder}
             alt="No image"
             width={500}
