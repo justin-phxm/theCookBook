b36269d3fc034f712b07d4111d3b6e8e50e50da9
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-07 15:57:56 -0600

    Add Recipe Item

--- the-cook-book/src/components/recipe/recipeHolder.tsx
+++ the-cook-book/src/components/recipe/recipeHolder.tsx
@@ -1,26 +1,22 @@
 import React from 'react'
 import {AiOutlinePlus} from 'react-icons/ai'
+import IFoodItem from '../FoodInterface'
+import RecipeItem from './RecipeItem'
 
-export default function recipeHolder() {
+export default function recipeHolder({foodItemArray}: {foodItemArray: IFoodItem[]}) {
   return (
     <div className=" bg-[#BCE3B2] w-full h-full p-3 rounded-lg">
-      <div className="bg-slate-200 h-full p-4 rounded-lg">
+      <div className="bg-slate-200 h-full w-full p-4 rounded-lg">
         <div className="flex flex-row justify-between">
           <h1 className=' font-bold select-none text-xl'>
             Recipes
           </h1>
           <AiOutlinePlus className="text-2xl z-10 font-semibold hover:bg-slate-300 hover:rounded-md cursor-pointer"/>
         </div>
-        <div className="overflow-auto">
-          <div>Item1</div>
-          <div>Item1</div>
-          <div>Item1</div>
-          <div>Item1</div>
-          <div>Item1</div>
-          <div>Item1</div>
-          <div>Item1</div>
-          <div>Item1</div>
-          <div>Item1</div>
+        <div className=" overflow-y flex flex-col h-full">
+          {foodItemArray.map((FoodItem:IFoodItem) => (
+              <RecipeItem FoodItem={FoodItem} key={FoodItem.id} />
+          ))}
         </div>
       </div>
     </div>
