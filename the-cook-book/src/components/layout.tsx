0f7c0a28592222f94c6e29942a60c4e257f96546
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-17 19:42:47 -0600

    update login styling

--- the-cook-book/src/components/layout.tsx
+++ the-cook-book/src/components/layout.tsx
@@ -6,31 +6,31 @@ import Footer from './footer';
 export const siteTitle = 'theCookBook';
 export default function Layout({children} : {children: React.ReactNode}) {
   return (
-    <div>
-        <Head>
-            <link rel="icon" href="/favicon.ico" />
-            <meta
-            name="description"
-            content="Save all your recipes with theCookBook"
-            />
-            {/* wtf is this below */}
-            <meta
-            property="og:image"
-            content={`https://og-image.vercel.app/${encodeURI(
-                siteTitle,
-            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-            />
-            <meta name="og:title" content={siteTitle} />
-            <meta name="twitter:card" content="summary_large_image" />
-        </Head>
-        <main className='min-h-screen bg-[#F4FFF1] overflow-hidden relative'>
-        <div className=" w-5/6 m-auto">
-            <Circle />
-            <NavBar />
-            {children}
-            <Footer />
-        </div>
-        </main>
+  <>
+    <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+        name="description"
+        content="Save all your recipes with theCookBook"
+        />
+        {/* wtf is this below */}
+        <meta
+        property="og:image"
+        content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle,
+        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+    </Head>
+    <main className='min-h-screen bg-[#F4FFF1] overflow-hidden relative'>
+    <div className=" w-5/6 m-auto">
+        <Circle />
+        <NavBar />
+        {children}
+        <Footer />
     </div>
+    </main>
+  </>
   );
 }
