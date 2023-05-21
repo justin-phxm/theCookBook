ba3758770fc26259e9b09cb43557b919eae2a63b
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-21 13:38:44 -0600

    add prettier

--- the-cook-book/src/components/recipe/recipeHolder.tsx
+++ the-cook-book/src/components/recipe/recipeHolder.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import {AiOutlinePlus} from 'react-icons/ai'
-import IFoodItem from '../FoodInterface'
-import RecipeItem from './RecipeItem'
+import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import IFoodItem from "../FoodInterface";
+import RecipeItem from "./RecipeItem";
 
-export default function recipeHolder(
-  {foodItemArray, setFoodItem}: 
-  {foodItemArray: IFoodItem[], setFoodItem: React.Dispatch<React.SetStateAction<IFoodItem>>}
-  ) {
+export default function recipeHolder({
+  foodItemArray,
+  setFoodItem,
+}: {
+  foodItemArray: IFoodItem[];
+  setFoodItem: React.Dispatch<React.SetStateAction<IFoodItem>>;
+}) {
   return (
-    <div className="bg-slate-200 h-max w-full p-4 rounded-lg">
+    <div className="bg-slate-200 h-full max-h-full w-full p-4 rounded-lg flex flex-col">
       <div className="flex flex-row justify-between">
-        <h1 className=' font-bold select-none text-xl'>
-          Recipes
-        </h1>
-        <AiOutlinePlus className="text-2xl z-10 font-semibold hover:bg-slate-300 hover:rounded-md cursor-pointer"/>
+        <h1 className=" font-bold select-none text-xl">Recipes</h1>
+        <AiOutlinePlus className="text-2xl z-10 font-semibold hover:bg-slate-300 hover:rounded-md cursor-pointer" />
       </div>
-      <ul className=" flex flex-col h-full">
-        {foodItemArray.map((FoodItem:IFoodItem) => (
-            <>
-            <RecipeItem FoodItem={FoodItem} key={FoodItem.id} setFoodItem={setFoodItem}/>
-            {/* <RecipeItem FoodItem={FoodItem} key={FoodItem.id} setFoodItem={setFoodItem}/> */}
-            </>
-            
+      <ul className=" flex flex-col overflow-y-auto">
+        {foodItemArray.map((FoodItem: IFoodItem) => (
+          <li key={FoodItem.id}>
+            <RecipeItem FoodItem={FoodItem} setFoodItem={setFoodItem} />
+            <RecipeItem FoodItem={FoodItem} setFoodItem={setFoodItem} />
+          </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
--- the-cook-book/src/components/recipe/recipeLayout.tsx
+++ the-cook-book/src/components/recipe/recipeLayout.tsx
@@ -1,57 +1,60 @@
-import Head from 'next/head';
-import Circle from '../circularDecoration';
-import NavBar from '../recipe/recipeNavBar';
-import RecipeHolder from './recipeHolder';
-import CookingArea from './cookingArea';
-import RecipeDetails from './recipeDetails';
-import RecentRecipeHolder from './recentRecipeHolder';
-import foodData from '../../../public/foodData.json' assert {type: "json"}
-import { useState } from 'react';
-import FoodInterface from '../FoodInterface';
+import Head from "next/head";
+import Circle from "../circularDecoration";
+import NavBar from "../recipe/recipeNavBar";
+import RecipeHolder from "./recipeHolder";
+import CookingArea from "./cookingArea";
+import RecipeDetails from "./recipeDetails";
+import RecentRecipeHolder from "./recentRecipeHolder";
+import foodData from "../../../public/foodData.json" assert { type: "json" };
+import { useState } from "react";
+import FoodInterface from "../FoodInterface";
 
-export const siteTitle = 'theCookBook';
+export const siteTitle = "theCookBook";
 
-export default function Layout({children} : {children: React.ReactNode}) {
-  const [foodItem, setFoodItem] = useState<FoodInterface>(foodData[0])
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const [foodItem, setFoodItem] = useState<FoodInterface>(foodData[0]);
   return (
     <>
       <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
+        <link rel="icon" href="/favicon.ico" />
+        <meta
           name="description"
           content="Save all your recipes with theCookBook"
-          />
-          {/* wtf is this below */}
-          <meta
+        />
+        {/* wtf is this below */}
+        <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-              siteTitle,
+            siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-          />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="twitter:card" content="summary_large_image" />
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <main className='h-screen bg-[#F4FFF1] overflow-hidden relative'>
+      <main className="h-screen bg-[#F4FFF1] overflow-hidden relative">
         <div className="h-screen w-11/12 m-auto flex flex-col pb-2">
-            <Circle />
-            <div>
-              <NavBar />
-            </div>
-            <div className="grid grid-cols-12 gap-2 h-full pb-2 z-10">
-              <section className="hidden lg:block lg:col-span-2 bg-[#BCE3B2] rounded-lg p-2 overflow-y-auto">
-                <RecipeHolder foodItemArray={foodData} setFoodItem={setFoodItem} />
-              </section>
-              <section className="col-span-8 lg:col-span-7 bg-[#BCE3B2] h-full max-h-full rounded-lg p-2">
-                <CookingArea foodItem={foodItem}/>
-              </section>
-              <section className="col-span-4 lg:col-span-3 bg-[#BCE3B2] rounded-lg p-2 overflow-auto">
-                <RecipeDetails />
-              </section>
-            </div>
-            {/* <div className=" h-1/4 bg-[#BCE3B2] rounded-lg p-2"> */}
-              {/* <RecentRecipeHolder foodItemArray={foodData} setFoodItem={setFoodItem} /> */}
-            {/* </div> */}
-            {children}
+          <Circle />
+          <div>
+            <NavBar />
+          </div>
+          <div className="grid grid-cols-12 gap-2 h-full pb-2 z-10">
+            <section className="hidden lg:block lg:col-span-2 bg-[#BCE3B2] rounded-lg p-2 ">
+              <RecipeHolder
+                foodItemArray={foodData}
+                setFoodItem={setFoodItem}
+              />
+            </section>
+            <section className="col-span-8 lg:col-span-7 bg-[#BCE3B2] h-full max-h-full rounded-lg p-2">
+              <CookingArea foodItem={foodItem} />
+            </section>
+            <section className="col-span-4 lg:col-span-3 bg-[#BCE3B2] rounded-lg p-2 overflow-auto">
+              <RecipeDetails />
+            </section>
+          </div>
+          {/* <div className=" h-1/4 bg-[#BCE3B2] rounded-lg p-2"> */}
+          {/* <RecentRecipeHolder foodItemArray={foodData} setFoodItem={setFoodItem} /> */}
+          {/* </div> */}
+          {children}
         </div>
       </main>
     </>
