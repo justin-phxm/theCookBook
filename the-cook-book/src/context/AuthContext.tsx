ced91fe58c2baa835f701a97756fec5bf9b66b61
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-21 17:47:15 -0600

    formatting

--- the-cook-book/src/context/AuthContext.tsx
+++ the-cook-book/src/context/AuthContext.tsx
@@ -1,48 +1,53 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { auth} from '../firebase'
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth'
+import React, { useContext, useState, useEffect } from "react";
+import { auth } from "../firebase";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 
-const AuthContext = React.createContext(useAuth)
+const AuthContext = React.createContext(useAuth);
 
 export function useAuth() {
-    return useContext(AuthContext)
+  return useContext(AuthContext);
 }
 
-export function AuthProvider({ children } : {children: React.ReactNode}) {
-    const [currentUser, setCurrentUser] = useState(null)
-    const [loading, setLoading] = useState(true)
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    function signup(email : string, password : string) {
-        createUserWithEmailAndPassword(auth, email, password)
-        return
-    }
+  function signup(email: string, password: string) {
+    createUserWithEmailAndPassword(auth, email, password);
+    return;
+  }
 
-    function login(email : string , password : string) {
-        return signInWithEmailAndPassword(auth, email, password)
-    }
+  function login(email: string, password: string) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
 
-    function logout() {
-        return signOut(auth)
-    }
+  function logout() {
+    return signOut(auth);
+  }
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, async user => {
-            setCurrentUser(user)
-            setLoading(false)
-        })
-        return unsubscribe
-    }, [])
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
 
-    const value = {
-        currentUser,
-        login,
-        signup,
-        logout,
-    }
+  const value = {
+    currentUser,
+    login,
+    signup,
+    logout,
+  };
 
-    return (
-        <AuthContext.Provider value={value}>
-            {!loading && children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
--- the-cook-book/src/pages/login.tsx
+++ the-cook-book/src/pages/login.tsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import Login from '@/components/Login'
-import Head from 'next/head'
+import React from "react";
+import Login from "@/components/Login";
+import Head from "next/head";
+import Link from "next/link";
 
 export default function login() {
-  const siteTitle = 'Login/Register'
+  const siteTitle = "Login/Register";
   return (
-      <>        
+    <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
-        name="description"
-        content="Save all your recipes with theCookBook"
+          name="description"
+          content="Save all your recipes with theCookBook"
         />
         {/* wtf is this below */}
         <meta
-        property="og:image"
-        content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
@@ -25,13 +26,15 @@ export default function login() {
       </Head>
       <div className="bg-gradient-to-r from-[#A7E4AF] to-[#F3E699] h-screen flex justify-center items-center">
         <div className="w-1/2 bg-white rounded-xl p-12">
-          <a className='text-lg font-bold hover:opacity-50 flex justify-center'
-          href="/">
+          <Link
+            className="text-lg font-bold hover:opacity-50 flex justify-center"
+            href="/"
+          >
             TheCookBook
-          </a>
-            <Login/>
-          </div>
+          </Link>
+          <Login />
+        </div>
       </div>
-  </>
-  )
+    </>
+  );
 }
