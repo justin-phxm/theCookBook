2017fa355eab61d604e826a805b13b5e7e7392d0
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-24 16:32:58 -0600

    foodData is updated from DB

--- the-cook-book/src/context/FoodContext.tsx
+++ the-cook-book/src/context/FoodContext.tsx
@@ -1,6 +1,7 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import FoodInterface from "@/lib/FoodInterface";
 import foodData from "../../public/foodData.json" assert { type: "json" };
+import { DatabaseProvider } from "@/lib/firestore";
 interface FoodContextType {
   foods: FoodInterface[];
   loading: boolean;
@@ -21,6 +22,13 @@ export const FoodProvider = ({ children }: { children: React.ReactNode }) => {
   const [loading, setLoading] = React.useState<boolean>(true);
   const [error, setError] = React.useState<string | null>(null);
   const [foodItem, setFoodItem] = React.useState<FoodInterface>(foods[0]);
+  // console.log(foods);
+  const { readDB } = DatabaseProvider();
+
+  useEffect(() => {
+    readDB().then((data) => setFood(data));
+  }, []);
+
   return (
     <FoodContext.Provider
       value={{ foods, loading, error, setFood, foodItem, setFoodItem }}
