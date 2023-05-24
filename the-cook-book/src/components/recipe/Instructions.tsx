a1e1f34d9d757a7da6bec8388aff524d9c09c77c
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-24 17:57:21 -0600

    changed naming update styling

--- the-cook-book/src/components/recipe/Instructions.tsx
+++ the-cook-book/src/components/recipe/Instructions.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import IFoodItem from "../../lib/FoodInterface";
 import { useFood } from "@/context/FoodContext";
 export default function Instructions() {
-  const { foodItem } = useFood();
+  const { currentFoodItem } = useFood();
   return (
     <div className="bg-white rounded-md h-full p-2 max-w-3xl">
       <div className=" font-bold text-lg text-">Instructions:</div>
       <ol className=" list-decimal font-normal max-h-[45rem]  overflow-y-auto list-inside ">
-        {foodItem.instructions?.map((instruction, index) => (
+        {currentFoodItem.instructions?.map((instruction, index) => (
           <li className="py-0.5" key={index}>
             {instruction}
           </li>
