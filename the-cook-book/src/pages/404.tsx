00f6f0d6016b3542925c0368b8ed0491a33b1aa9
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-24 19:53:27 -0600

    add 404

---
+++ the-cook-book/src/pages/404.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/facePage/layout";
+export default function PageNotFound() {
+  return (
+    <Layout>
+      <div className="flex justify-center text-center text-4xl">
+        404 Error -- Page not found
+      </div>
+    </Layout>
+  );
+}
