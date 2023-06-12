a135d208d0fe0052e5663be0f1ec99fe04cd2fdb
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-06-12 10:29:12 -0600

    add analytics

--- the-cook-book/src/pages/_app.tsx
+++ the-cook-book/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AuthProvider } from "@/context/AuthContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Outfit } from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/react";
 const outfit = Outfit({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -10,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <AuthProvider>
       <main className={outfit.className}>
         <Component {...pageProps} />
+        <Analytics />
       </main>
     </AuthProvider>
   );
