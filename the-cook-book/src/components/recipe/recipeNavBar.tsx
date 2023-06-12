64f364a573d84c6a0aed62b83259c4486de2c17b
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-06-12 09:42:00 -0600

    demoMode notification

--- the-cook-book/src/components/recipe/recipeNavBar.tsx
+++ the-cook-book/src/components/recipe/recipeNavBar.tsx
@@ -70,50 +70,61 @@ export default function NavBar() {
       border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-inherit 
       dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
-          <li>
-            <Link
-              href="#"
-              className="block py-2 pl-3 pr-4 rounded md:bg-transparent
-           md:text-green-700 md:p-0 dark:text-white md:dark:text-green-500"
-              aria-current="page"
-            >
-              <FaUserAlt />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#"
-              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >
-              <HiCollection />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#"
-              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >
-              <HiMagnifyingGlass />
-            </Link>{" "}
-          </li>
           {currentUser ? (
-            <li>
-              <button
-                onClick={logoutHandler}
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                {currentUser.email} (Sign Out)
-              </button>
-            </li>
+            <>
+              {" "}
+              <li>
+                <Link
+                  href="#"
+                  className="block py-2 pl-3 pr-4 rounded md:bg-transparent
+           md:text-green-700 md:p-0 dark:text-white md:dark:text-green-500"
+                  aria-current="page"
+                >
+                  <FaUserAlt />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  <HiCollection />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  <HiMagnifyingGlass />
+                </Link>{" "}
+              </li>
+              <li>
+                <button
+                  onClick={logoutHandler}
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  {currentUser.email} (Sign Out)
+                </button>
+              </li>
+            </>
           ) : (
-            <li>
-              <Link
-                href="/login"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Sign In
-              </Link>
-            </li>
+            <>
+              {" "}
+              <li>
+                <p className="block text-gray-900 font-light  dark:text-white ">
+                  (DemoMode)
+                </p>
+              </li>
+              <li>
+                <Link
+                  href="/login"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Sign In
+                </Link>
+              </li>
+            </>
           )}
         </ul>
       </div>
