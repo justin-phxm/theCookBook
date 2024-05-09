89460ebe82292f2a446806a58a486b56ebf7db0a
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-05-09 16:07:12 -0600

    update contact page

--- the-cook-book/src/app/contact/page.tsx
+++ the-cook-book/src/app/contact/page.tsx
@@ -1,15 +1,14 @@
-import Head from "next/head";
-import Layout from "../components/facePage/layout";
+import Layout from "../../components/facePage/layout";
 import Image from "next/image";
 import aboutImage from "@/12.png";
-
-export default function contact() {
+export const metadata = {
+  title: "Contact",
+  description: "Contact TheCookBook",
+};
+export default function Page() {
   return (
     <>
       <Layout>
-        <Head>
-          <title>Contact</title>
-        </Head>
         <div className="text-3xl font-bold">Contact</div>
         <Image src={aboutImage} alt="" width={1000} />
       </Layout>
